50a5c1d8cf1143c4baff599baa48eccfa9fcc0e8
Karen Etheridge <ether@cpan.org>
2014-04-23 09:43:07 -0700

    bump version to 1.002001

--- lib/JSON/MaybeXS.pm
+++ lib/JSON/MaybeXS.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 use base qw(Exporter);
 
-our $VERSION = '1.002000';
+our $VERSION = '1.002001';
 
 sub _choose_json_module {
     return 'Cpanel::JSON::XS' if $INC{'Cpanel/JSON/XS.pm'};
