fbfb6235c411c5bdfe3c1a0b6652321dc23c8108
Karen Etheridge <ether@cpan.org>
2014-10-07 09:39:10 -0700

    make bump

--- lib/JSON/MaybeXS.pm
+++ lib/JSON/MaybeXS.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 use base qw(Exporter);
 
-our $VERSION = '1.002002';
+our $VERSION = '1.002003';
 
 sub _choose_json_module {
     return 'Cpanel::JSON::XS' if $INC{'Cpanel/JSON/XS.pm'};
