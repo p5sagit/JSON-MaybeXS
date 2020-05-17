02b1ee35a374abb39494e504f9847571f74cd4db
Karen Etheridge <ether@cpan.org>
2020-05-17 16:05:48 -0700

    update documentation to reflect requirements change from 1.004001

--- lib/JSON/MaybeXS.pm
+++ lib/JSON/MaybeXS.pm
@@ -121,7 +121,8 @@ JSON::MaybeXS - Use L<Cpanel::JSON::XS> with a fallback to L<JSON::XS> and L<JSO
 =head1 DESCRIPTION
 
 This module first checks to see if either L<Cpanel::JSON::XS> or
-L<JSON::XS> is already loaded, in which case it uses that module. Otherwise
+L<JSON::XS> (at at least version 3.0)
+is already loaded, in which case it uses that module. Otherwise
 it tries to load L<Cpanel::JSON::XS>, then L<JSON::XS>, then L<JSON::PP>
 in order, and either uses the first module it finds or throws an error.
 
