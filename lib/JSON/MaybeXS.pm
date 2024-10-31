fac35c24b0b2a7542df5d2fa8a01b2881b1129b4
Karen Etheridge <ether@cpan.org>
2024-10-31 14:50:31 -0700

    comment why we don't use builtin::compat::is_bool

--- lib/JSON/MaybeXS.pm
+++ lib/JSON/MaybeXS.pm
@@ -55,6 +55,9 @@ use Scalar::Util ();
 use constant HAVE_BUILTIN => "$]" >= 5.036;
 use if HAVE_BUILTIN, experimental => 'builtin';
 
+# note: unlike builtin::compat::is_bool on older perls, we do not accept
+# dualvar(0,"") or dualvar(1,"1") because JSON::PP and Cpanel::JSON::XS
+# do not encode these as booleans.
 sub is_bool {
   die 'is_bool is not a method' if @_ > 1;
 
